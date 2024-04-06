52141ae990bed1e185d3462bbfb895593941aaf7
Jhonnys DS <jdeavilasalgado@gmail.com>
2024-04-06 11:28:04 -0500

    enrutamiento

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/public/auth/login/login.component';
+import { RegisterComponent } from './components/public/auth/register/register.component';
+export const routes: Routes = [
+    {
+        path: 'auth',
+        children:[
+            {
+                path: 'login',
+                component: LoginComponent
+            },
+            {
+                path: 'register',
+                component: RegisterComponent
+            }
+        ]
+    }
 
-export const routes: Routes = [];
+];
